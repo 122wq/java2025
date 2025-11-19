1dba4bf6f70b3c944cdcc4ca3976c17371aac22a
122wq <106130176+122wq@users.noreply.github.com>
2025-11-19 23:48:03 +0000

    new file:   WaterFlowingTest.class new file:   WaterFlowingTest.java new file:   waterFlowing.class modified:   waterFlowing.java

--- waterFlowing.java
+++ waterFlowing.java
@@ -7,28 +7,26 @@ public class waterFlowing
 
     public static boolean canFlowOff(int[][] map, int row, int col)
     {
-        
-        if (row > 0 || col > 0 || row < map[0].length - 1 || col < map.legnth - 1)
+		int current = map[row][col];
+        if (row > 0 && col > 0 && row < map[0].length - 1 && col < map.length - 1)
         {
+			//Up
             if (map[row - 1][col] < current && canFlowOff(map, row - 1, col)) {
-       	 	return true;
+       	 		return true;
     		}
 
-    		// DOWN
-    		if (map[row + 1][col] < current &&
-        		canFlowOff(map, row + 1, col)) {
+    		//Down
+    		if (map[row + 1][col] < current && canFlowOff(map, row + 1, col)) {
         		return true;
     		}
 
-    		// LEFT
-    		if (map[row][col - 1] < current &&
-        		canFlowOff(map, row, col - 1)) {
+    		//Left
+    		if (map[row][col - 1] < current && canFlowOff(map, row, col - 1)) {
         		return true;
     		}
 
-    		// RIGHT
-    		if (map[row][col + 1] < current &&
-        		canFlowOff(map, row, col + 1)) {
+    		//Right
+    		if (map[row][col + 1] < current && canFlowOff(map, row, col + 1)) {
 	        	return true;
 	    	}
 			return false;
@@ -37,17 +35,18 @@ public class waterFlowing
     }
     public static void main(String[] args) {
         int[][] map = {
-			{100, 99, 200, 200, 200, 200, 200, 200, 200, 200}, 
-			{200, 98, 200, 200, 200, 200, 200, 200, 200, 200},
-			{200, 97, 96, 200, 200, 200, 200, 200, 200, 200},
-			{200, 200, 95, 200, 200, 200, 85, 84, 83, 200},
-			{200, 200, 94, 93, 92, 200, 86, 200, 82, 200},
-			{200, 150, 200, 200, 91, 200, 87, 200, 81, 200},
-			{200, 200, 200, 200, 90, 89, 88, 200, 80, 200},
-			{200, 150, 100, 200, 200, 200, 200, 200, 79, 200},
-			{200, 200, 200, 200, 200, 200, 200, 200, 78, 77},
-			{200, 98, 200, 200, 200, 200, 200, 200, 200, 76}		
-	    };
-        System.out.println(canFlowOff(map, 3, 2));
+		{100, 200, 200, 200, 200, 200, 200, 200, 200, 200}, 
+		{200, 200, 200, 200, 200, 200, 200, 200, 200, 200},
+		{200, 10, 200, 200, 200, 200, 200, 200, 200, 200},
+		{200, 11, 10, 200, 200, 6, 85, 84, 83, 200},
+		{200, 200, 14, 15, 59, 200, 86, 200, 82, 200},
+		{200, 11, 12, 200, 200, 200, 87, 200, 81, 200},
+		{200, 10, 200, 200, 90, 89, 88, 200, 200, 200},
+		{200, 9, 8, 200, 200, 200, 200, 200, 200, 200},
+		{200, 200, 7, 200, 200, 200, 200, 200, 200, 200},
+		{200, 98, 6, 200, 200, 200, 200, 200, 200, 200}
+	};
+ 
+	System.out.println(canFlowOff(map, 4, 4));
     }
 }
