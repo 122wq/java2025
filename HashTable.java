1e0cd2445a7fea071c01384f2f1e1e0961b6d7e5
122wq <106130176+122wq@users.noreply.github.com>
2026-02-20 23:59:18 +0000

    modified:   .vscode/settings.json modified:   HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -2,23 +2,100 @@ import java.util.*;
 import java.io.*;
 
 public class HashTable {
+    private ArrayList<String>[] arr;
+    private int hashSize;
+
+    public HashTable ()
+    {
+        arr = new ArrayList[67];
+        hashSize = 0;
+    }
 
 // Methods you have to supply:
 //
-//  public void put(String key) {
-//  }
+    public void put(String key) {
+        int hash = key.hashCode();
+        hash = hash % arr.length;
+
+        if (arr[hash] == null)
+        {
+            arr[hash] = new ArrayList<String>();
+        }
+        arr[hash].add(key);
+        hashSize++;
+
+        if ((hashSize / 100.00) > 0.67)
+        {
+            ArrayList<String>[] newArr = new ArrayList[arr.length * 2];
+            hashSize = 0;
+            Iterator i = new MyIterator();
+            int newHash;
+            String newKey;
+            
+            while (i.hasNext())
+            {
+                newKey = (String) i.next();
+                newHash = (newKey.hashCode()) % arr.length;
+                if (newArr[newHash] == null)
+                {
+                    newArr[newHash] = new ArrayList<String>();
+                }
+                newArr[newHash].add(key);
+                hashSize++;
+                
+            }
+            arr = newArr;   
+            
+        }
+    }
+
+    public String get(String key){
+        int hash = key.hashCode() % arr.length;
+    
+        //look through the arr[hash] arrayList (if one exists) and return key if you find it. return null if it's not in there
+        if (arr[hash] != null)
+        {
+            for (int i = 0; i < arr[hash].size(); i++)
+            {
+                if (arr[hash].get(i).equals(key))
+                {
+                    return key;
+                }
+            }
+        }
+        return null;
+    }
+
+    public String remove(String key){
+        int hash = key.hashCode() % arr.length;
+        //look through the arr[hash] arrayList (if one exists) and return key if you find it. return null if it's not in there
+        if (arr[hash] != null)
+        {
+            for (int i = 0; i < arr[hash].size(); i++)
+            {
+                if (arr[hash].get(i).equals(key))
+                {
+                    arr[hash].remove(i);
+                    hashSize--;
+                    return key;
+                }
+            }
+        }
+        return null;
+
+	}
 //
-//  public String get(String key) {
-//  }
+    public Iterator keys() {
+        return new MyIterator();
+    }
 //
-//  public String remove(String key){
-//	}
-//
-//  public Iterator keys() {
-//  }
-//
-//  public void print(){
-//	}
+    public void print(){
+        Iterator i = new MyIterator();
+        while (i.hasNext())
+        {
+            System.out.println(i.next());
+        }
+	}
 	/**
 	 * Loads this HashTable from a file named "Lookup.dat".
 	 */
@@ -51,7 +128,7 @@ public class HashTable {
                     System.out.println("Error in input file");
                     System.exit(1);
                 }
-                put(key, value);
+                put(key);
             }
         }
         catch (IOException e) {
@@ -115,24 +192,84 @@ public class HashTable {
 
     private class MyIterator implements Iterator
     {
+        private int posArr;
+        private int posList; 
 
+        public MyIterator()
+        {
+            posArr = 0;
+            posList = -1;
+        }
         @Override
         public boolean hasNext() {
             // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+            int currPos = posArr;
+            if (posArr < arr.length)
+            {
+                if (arr[posArr] != null && (posList + 1) < arr[posArr].size())
+                {
+                    return true;
+                } 
+                else
+                {
+                    //increase currPos if the next array element is null, until it finds a non-null element or it searches the entire array (no next value)
+                    while ((currPos + 1) < arr.length && (arr[currPos + 1] == null || arr[currPos + 1].isEmpty()))
+                    {
+                        currPos++;
+                    }
+                    //if currPos + 1 is the arr length, it means that the entire array was search and there is no next value that exist, so returns true
+                    return((currPos + 1) != arr.length);
+                }
+            }
+            return false;
         }
 
         @Override
         public Object next() {
             // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'next'");
+            int currPos = posArr;
+            if (hasNext())
+            {
+                if (posArr < arr.length)
+                {
+                    if (arr[posArr] != null && (posList + 1) < arr[posArr].size())
+                    {
+                        posList++;
+                        return arr[posArr].get(posList);
+                    } 
+                    else
+                    {
+                        //increase currPos if the next array element is null, until it finds a non-null element or it searches the entire array (no next value)
+                        while ((currPos + 1) < arr.length && (arr[currPos + 1] == null || arr[currPos + 1].isEmpty()))
+                        {
+                            currPos++;
+                        }
+                        posArr = currPos + 1;
+                        posList = 0;
+                        //if currPos + 1 is the arr length, it means that the entire array was search and there is no next value that exist, so returns true
+                        if (posArr < arr.length)
+                        {
+                            return arr[posArr].get(0);
+                        }
+                    }
+                }
+            }
+            return null;
         }
 
         @Override
         public void remove()
         {
-            
-        
+            return;
         }
+    }
+
+}
+
+ class HashTableTest
+{
+    public static void main(String[] args)
+    {
+        
     }
 }
